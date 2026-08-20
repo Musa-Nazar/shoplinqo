b2b756a28f8a982dac690ae01870e81f615dca4a
Musa Nazar <musa120509@gmail.com>
2026-08-20 14:53:31 +0100

    Fixed pos page

--- app/(dashboard)/pos/page.tsx
+++ app/(dashboard)/pos/page.tsx
@@ -2,12 +2,15 @@ import PosHeaaderSection from "@/components/pos/PosHeaaderSection";
 import PosItems from "@/components/pos/PosItems";
 import PosNavSection from "@/components/pos/PosNavSection";
 import PosSearchSection from "@/components/pos/PosSearchSection";
+import { Suspense } from "react";
 
 const POS: React.FC = () => {
   const xml: React.ReactNode = (
     <main className="h-dvh bg-(--special-gray) overflow-auto pb-38">
       <PosHeaaderSection />
-      <PosNavSection />
+      <Suspense>
+        <PosNavSection />
+      </Suspense>
       <PosSearchSection />
       <PosItems />
     </main>
